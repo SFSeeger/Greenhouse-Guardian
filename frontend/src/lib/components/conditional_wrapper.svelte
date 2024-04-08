c1fedce9bf06e7e30d7b9a2a623ca3f3de6730c2
Simon Felix Seeger <simon.f.seeger@gmx.de>
2024-04-08 14:53:24 +0200

    Svelte API intigration

---
+++ frontend/src/lib/components/conditional_wrapper.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let condition: boolean = false;
+    export let tag: string = "a";
+</script>
+
+{#if condition && tag}
+    <svelte:element this={tag} {...$$restProps}>
+        <slot></slot>
+    </svelte:element>
+{:else}
+    <slot></slot>
+{/if}
---
+++ frontend/src/routes/devices/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import Card from '$lib/components/card.svelte';
+    
+    export let data: PageData;
+</script>
+
+<h1>Devices</h1>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+    {#each data.devices as device}
+        <Card title={device.name} href="/device/{device.id}">
+            {device.name}
+        </Card>
+    {/each}
+</div>
