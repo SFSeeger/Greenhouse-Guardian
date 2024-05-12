687df72d7ad9107c1950650e284ec38f2dc6126f
Simon Felix Seeger <simon.f.seeger@gmx.de>
2024-05-12 10:43:29 +0200

    Add error logging

--- api/entrypoints/prod.sh
+++ api/entrypoints/prod.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 source /app/base.sh
 
-gunicorn -b 0.0.0.0:80 api.wsgi:application
+gunicorn -b 0.0.0.0:80 api.wsgi:application \
+    --workers 4 \
+    --timeout 120 \
+    --log-level warning \
+    --access-logfile - \
+    --error-logfile -
